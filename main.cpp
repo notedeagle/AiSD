4dd33bd8664d02a87b0ff44a6d3adc6244a4589e
notedeagle <notedeagle@gmail.com>
2019-11-09 13:17:24 +0100

    Horner

--- main.cpp
+++ main.cpp
@@ -1,7 +1,37 @@
 #include "Horner.h"
 
-int main() {
+void horner() {
     Horner horner;
     horner.run();
+}
+
+int getInput() {
+    int choice;
+    cin >> choice;
+    return choice;
+}
+
+void displayMenu() {
+    cout << "Wybierz algorytm:" << endl;
+    cout << "1. Horner" << endl;
+    cout << "Wybor: " << endl;
+}
+
+int main() {
+    int choice;
+
+    do {
+        displayMenu();
+        choice = getInput();
+
+        switch (choice) {
+            case 1:
+                horner();
+                break;
+            default:
+                break;
+        }
+    } while (choice != 9);
+
     return 0;
 }
